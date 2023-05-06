b40497bde99994bec6b12de8648c5edbdc7f6004
Daniil <68585455+mrwhiteered@users.noreply.github.com>
2023-05-06 17:29:43 +0300

    6/05
    1. Статьи (просто для демонстрации)
    2. Чуть стилей цвета. надписи и тд

---
+++ JS/disertation.js
@@ -0,0 +1,40 @@
+let disirtationBtn = document.getElementById('item__diser');
+let statBtn = document.getElementById('item__stat');
+
+let listStats = document.getElementById('listStats');
+let listDiser = document.getElementById('diserContainer');
+
+const imgDis = document.getElementById('diser');
+const imgStat = document.getElementById('stat');
+
+function addLine() {
+    if (!imgDis.classList.contains('under')) {
+imgDis.classList.add('under');
+imgStat.classList.remove('under');
+listStats.classList.remove('showStats');
+listDiser.classList.add('showDiser');
+    } else {
+    imgDis.classList.remove('under');
+imgStat.classList.add('under');
+listStats.classList.add('showStats');
+listDiser.classList.remove('showDiser');
+    }
+}
+
+function addLine2() {
+    if (!imgStat.classList.contains('under')) {
+        imgStat.classList.add('under');
+imgDis.classList.remove('under');
+listStats.classList.add('showStats');
+listDiser.classList.remove('showDiser');
+    } else {
+    imgStat.classList.remove('under');
+    imgDis.classList.add('under');
+    listStats.classList.remove('showStats');
+    listDiser.classList.add('showDiser');
+    }
+}
+
+
+// добавление статей
+
